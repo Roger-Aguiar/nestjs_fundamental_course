527f24525d907657d4dcb1d30b601e3006879b0b
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2022-01-16 15:39:56 -0300

    It creates the post, update and delete methods.

--- src/controllers/coffees/coffees.controller.ts
+++ src/controllers/coffees/coffees.controller.ts
@@ -1,19 +1,32 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-    @Get()
-    findAll(){
-        return "Hello! This is the list of our coffees!";
-    }
-
-    @Get(':id')
-    findOne(@Param('id') id: string){
-        return `This action returns ${id} coffee!`;
-    }
-
     @Post()
+    @HttpCode(HttpStatus.CREATED)
     create(@Body() body){
         return body;
     }
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    read(){
+        return "Hello! This is the list of our coffees!";
+    }
+
+    @Put(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    update(@Param('id') param, @Body() body){
+        return `This operation updates the coffee ${param.id}.`;
+    }
+
+    @Delete(':id')    
+    delete(@Param('id') id: string){
+        return `This operation deletes the coffee ${id}.`;
+    }
+
+    @Get(':id')
+    readOne(@Param('id') id: string){
+        return `This action returns ${id} coffee!`;
+    }
+        
 }
